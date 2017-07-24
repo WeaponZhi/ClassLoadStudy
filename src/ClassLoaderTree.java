857908e3c0e2fde4ac5e44e0407b2783456c4880
WeaponZhi <584098488@qq.com>
2017-07-24 10:24:21 +0800

    initial commit

---
+++ src/ClassLoaderTree.java
@@ -0,0 +1,16 @@
+/**
+ * ClassLoaderTree 演示类加载器的树状组织结构
+ * author:张冠之
+ * time: 2017/7/24 10:22
+ * e-mail: guanzhi.zhang@sojex.cn
+ */
+
+public class ClassLoaderTree {
+    public static void main(String[] args) {
+        ClassLoader loader = ClassLoaderTree.class.getClassLoader();
+        while (loader != null) {
+            System.out.println(loader.toString());
+            loader = loader.getParent();
+        }
+    }
+}
